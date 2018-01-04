36b50ababd105bcd0d6709a23ee25a48242c7cc1
ljwinds <523119500@qq.com>
2018-01-04 17:35:53 +0800

    nothing changed

---
+++ NFVO/Controllers/NFVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFVO.Controllers
+{
+    public class NFVController : Controller
+    {
+        // GET: NFV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
